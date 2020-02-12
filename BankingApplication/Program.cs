d0d4cee0e8113c2291a6be1647560e39da9ad039
Armon Porter <max-student@maxtrain.com>
2020-02-12 16:32:22 -0500

    Add project files.

---
+++ BankingApplication/Program.cs
@@ -0,0 +1,15 @@
+﻿using BankingLibrary;
+using System;
+
+namespace BankingApplication {
+    class Program {
+        static void Main(string[] args) {
+            var checking = new Account();
+            var saving = new Account();
+
+            checking.Deposit(100);
+
+            checking.Withdraw(75);
+        }
+    }
+}
---
+++ BankingLibrary/Account.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace BankingLibrary {
+
+    public class Account {
+
+        public decimal Balance { get; set; }
+        public string AcctNbr { get; set; }
+        public string Description { get; set; }
+
+        public void Deposit(decimal amount) {
+            Balance += amount;
+        }
+
+        public void Withdraw(decimal amount) {
+            Balance -= amount;
+        }
+
+        public void Transfer(decimal amount, Account toAcct, Account fromAcct) {
+            fromAcct.Withdraw(amount);
+            toAcct.Deposit(amount);
+        }
+
+        public Account() {} //default constructor
+    }
+}
